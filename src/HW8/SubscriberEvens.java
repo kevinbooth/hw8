f3b4c45cb01b3e62b73473d8d85fceaa33814f5f
Kevin Booth <kdbooth97@gmail.com>
2019-03-30 15:35:39 -0400

    Implement notifyObserver even,odd, and threes subscriber classes

--- src/HW8/SubscriberEvens.java
+++ src/HW8/SubscriberEvens.java
@@ -2,4 +2,12 @@ package HW8;
 
 public class SubscriberEvens implements Observer {
 
+	public boolean notifyObserver(Event e) {
+		if (e.getEventNumber() % 2 == 0) {
+			String message = String.format("Event %d is even: %d", e.getEventNumber(), e.getEventData());
+			System.out.println(message);
+			return true;
+		}
+		else return false;
+	}
 }
--- src/HW8/SubscriberOdds.java
+++ src/HW8/SubscriberOdds.java
@@ -1,5 +1,13 @@
 package HW8;
 
 public class SubscriberOdds implements Observer {
-
+	
+	public boolean notifyObserver(Event e) {
+		if (e.getEventData() % 2 != 0) {
+			String message = String.format("Event %d is odd: %d", e.getEventNumber(), e.getEventData());
+			System.out.println(message);
+			return true;
+		}
+		else return false;
+	}
 }
