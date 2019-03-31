0046dbb21d6d49e2f3b01de57a6444ebe65d7555
Kevin Booth <kdbooth97@gmail.com>
2019-03-30 23:21:00 -0400

    Update Subscriber classes

--- src/HW8/SubscriberEvens.java
+++ src/HW8/SubscriberEvens.java
@@ -3,7 +3,7 @@ package HW8;
 public class SubscriberEvens implements Observer {
 
 	public boolean notifyObserver(Event e) {
-		if (e.getEventNumber() % 2 == 0) {
+		if (e.getEventData() % 2 == 0) {
 			String message = String.format("Event %d is even: %d", e.getEventNumber(), e.getEventData());
 			System.out.println(message);
 			return true;
--- src/HW8/SubscriberThrees.java
+++ src/HW8/SubscriberThrees.java
@@ -3,7 +3,7 @@ package HW8;
 public class SubscriberThrees implements Observer {
 
 	public boolean notifyObserver(Event e) {
-		if (e.getEventData() % 3 != 0) {
+		if (e.getEventData() % 3 == 0) {
 			String message = String.format("Event %d is divisible by three: %d", e.getEventNumber(), e.getEventData());
 			System.out.println(message);
 			return true;
